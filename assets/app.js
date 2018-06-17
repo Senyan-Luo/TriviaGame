fe75dee29030328d506a95b52d5c5c501b612f23
Charlotte Luo <sel152@ucsd.edu>
2018-06-17 10:18:24 -0700

    timer fixed

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,30 @@
 $(document).ready(function(){
-     let number = 60
+     let number = 61
      let intervalId
 
 $("#startButton").on("click", function(){
     $(this).hide()
-    var submitButton = $("<button>submit</butotn>")
-    $(submitButton).on("click", stop())
-    setInterval(decrement(), 1000)
-     if (number === 0){ 
-        stop()
-    }
-    $("#time-left").append("<h2>" + number + " seconds left" + "</h2>")
+    //var submitButton = $("<button>submit</butotn>")
+    //$(submitButton).on("click", stop())
+   intervalId = setInterval(decrement, 1000)
+  
+    })
 
-})
+   
 
 function decrement(){
-    number --
+    number--
+    $("#time-left").text("You have " + number + " seconds left")
+    console.log(number) 
+    if (number === 0){ 
+    stop()
+    }
 }
 
 function stop(){
-    //clearInterval(intervalId)
-    $("#time-left").html("<h2>"+ "Time is up!" + "</h2>")
+    clearInterval(intervalId)
+    $("#time-left").hide()
+    $("#time-up").html("<h2>"+ "Time is up!" + "</h2>")
     $("#q1").text("Correct answers")
 }
 
